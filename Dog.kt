8bb6f3f0ca9342aff8e205fef317791270e4aeb4
IlyaKirichenko <ikirichenko345@gmail.com>
2025-03-05 17:12:07 +0500

    Update Dog.kt

--- Dog.kt
+++ Dog.kt
@@ -1,17 +1,15 @@
-open class Dog(var name : String) {
-    var rost = 0.0
-    var okras : String = ""
-    var ves = 0.0
-    fun Proverka (rost : Double, ves : Double){
+open class Dog(var name: String) {
+    var rost: Double = 0.0
+    var okras: String = ""
+    var ves: Double = 0.0
+
+    fun Proverka(rost: Double, ves: Double) {
         when {
             (rost < 0) -> println("Не может быть отрицательного роста")
-            (ves < 0) ->  println("Не может быть отрицательного веса")
+            (ves < 0) -> println("Не может быть отрицательного веса")
+            else -> {
+                this.rost = rost
+                this.ves = ves
+            }
         }
     }
-
-        //вывод информации О СОБАКЕ
-   open fun Info_dog() {
-        println("кличка собаки: $name, рост собаки: $rost, вес собаки $ves")
-    }
-
-}
