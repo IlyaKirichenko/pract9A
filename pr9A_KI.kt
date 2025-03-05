f49f363a9b1a4fea85f3233bf01715763e57eca4
IlyaKirichenko <ikirichenko345@gmail.com>
2025-03-05 17:09:25 +0500

    Update pr9A_KI.kt

--- pr9A_KI.kt
+++ pr9A_KI.kt
@@ -1,22 +1,25 @@
-fun main () {
-
-    var dog1 = Slug_dog("Агрессивный", "Боб" ) //Создание объекта
-    println("собака №1:")
+fun main() {
+    val dog1 = Slug_dog("Агрессивный", "Боб") // Создание объекта
+    println("Собака №1:")
     println("Введите срок службы собаки:")
     dog1.sr_slug = readLine()!!.toInt()
     println("Введите характер собаки:")
+    dog1.haract = readLine()!!
 
-    var dog2 = Dog("Шарик")
-    println("собака №2:")
+    val dog2 = Dog("Шарик")
+    println("Собака №2:")
     println("Введите рост и вес собаки:")
-    dog2.Proverka(readln()!!.toDouble(),readln()!!.toDouble())
+    dog2.Proverka(readLine()!!.toDouble(), readLine()!!.toDouble())
+    dog2.ProverkaVesa() // Проверка соответствия веса и роста
 
-    var dog3 = Poroda("Лаки", "Шпиц")
-    println("собака №3:")
-    dog3.porod = readLine()!!.toString()
+    val dog3 = Poroda("Лаки", "Шпиц")
+    println("Собака №3:")
+    println("Введите породу собаки:")
+    dog3.porod = readLine()!!
     println("Введите окраску собаки:")
-    dog3.okras = readLine()!!.toString()
-        //Информация о собаке
+    dog3.okras = readLine()!!
+
+    // Информация о собаке
     dog1.Info_dog()
     dog2.Info_dog()
     dog3.Info_dog()
