61472a2375b89cf59d0daab94d5977839dcb1548
IlyaKirichenko <ikirichenko345@gmail.com>
2025-03-05 17:10:21 +0500

    Update Slug_dog.kt

--- Slug_dog.kt
+++ Slug_dog.kt
@@ -1,12 +1,10 @@
-class Slug_dog(var haract : String ,_name:String):Dog(_name) {
+class Slug_dog(var haract: String, _name: String) : Dog(_name) {
     var sr_slug = 0
-    fun Es (){
-        when sr_slug == {
-            1..8 -> println("")
-            9..13 ->
+
+    fun Es() {
+        when (sr_slug) {
+            in 1..8 -> println("Срок службы в пределах нормы")
+            in 9..13 -> println("Срок службы выше нормы")
+            else -> println("Некорректный срок службы")
         }
     }
-    override fun Info_dog(){
-        println("кличка собаки: $name, срок службы: $sr_slug, характер: $haract")
-    }
-}
